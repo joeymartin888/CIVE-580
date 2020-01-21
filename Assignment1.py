2b084fd6ac6ab1078a90993e3dab2e4cf0430e05
joeymartin888 <joeymartin888@gmail.com>
2020-01-20 17:13:17 -0800

    with plotting capabilities

--- Assignment1.py
+++ Assignment1.py
@@ -1,8 +1,14 @@
+import unicodedata
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 means=[0 for i in range(12)]
-jump=pd.read_csv("3B23P_Jump_Creek.csv") #load in csv
+jump=pd.read_csv("3B23P_Jump_Creek.csv") #load in csvs
+duncan=pd.read_csv("1022571_Duncan_Glenora.csv")
+months=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 conditions=["tmax_C","tmin_C","precip_mm"] 
+print("3B23P_Jump_Creek.csv")
 for t in conditions: # loop through conditions
 	print(t)
 	for x in range(0, 12): # loop through monthd
@@ -10,3 +16,40 @@ for t in conditions: # loop through conditions
 		temp.dropna() #remove NaNs
 		means[x]=temp.mean()
 	print(means)
+	tp=pd.Series(means, index=months)
+	tp.plot(kind='bar',rot=0)
+	plt.xlabel("Months")	
+	if t=="precip_mm":
+		plt.ylabel("Precipitation in mm")
+		tp.plot(title="Average Precipitation Monthly for Jump Creek")
+	else:
+		plt.ylabel("Temperature in degrees Celsius")
+		if t=="tmax_C":
+			tp.plot(title="Maximum Monthly Temperatures for Jump Creek")
+		else:
+			tp.plot(title="Minimum Monthly Temperatures for Jump Creek")	
+	plt.show()
+
+print("1022571_Duncan_Glenora.csv")
+for t in conditions: # loop through conditions
+	print(t)
+	for x in range(0, 12): # loop through monthd
+		temp=duncan[t].where(duncan['month']==(x+1)) # python zero-index
+		temp.dropna() #remove NaNs
+		means[x]=temp.mean()
+	print(means)
+	p=pd.Series(means, index=months)
+	tp.plot(kind='bar',rot=0)
+	plt.xlabel("Months")	
+	if t=="precip_mm":
+		plt.ylabel("Precipitation in mm")
+		tp.plot(title="Average Precipitation Monthly for Duncan Glenora")
+	else:
+		plt.ylabel("Temperature in degrees Celsius")
+		if t=="tmax_C":
+			tp.plot(title="Maximum Monthly Temperatures for Duncan Glenora")
+		else:
+			tp.plot(title="Minimum Monthly Temperatures for Duncan Glenora")	
+	plt.show()
+
+
