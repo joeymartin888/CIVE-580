909ce689d9cc21d5f53d743c41ba2cbdbb545b13
joeymartin888 <joeymartin888@gmail.com>
2020-02-01 16:53:06 -0800

    Q1 plots done, Q2 in progress

--- Assignment2/Assignment2.py
+++ Assignment2/Assignment2.py
@@ -8,45 +8,88 @@ Created on Wed Jan 22 11:10:23 2020
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 """Sort the dataframes first into useable matrices!!"""
 
 pin=pd.read_csv("/home/josmarti/Code/CIVE-580/Assignment1/1022571_Duncan_Glenora.csv")
 qin=pd.read_csv("Cowichan_Duncan_monthlyQ.csv")
-psorted=np.zeros((len(pin["Unnamed: 0"]),len(pin.columns)-1))
-qsorted=np.zeros((len(qin["Unnamed: 0"]),len(qin.columns)))
+psorted=pd.DataFrame(columns = ["water year", "month", "precip_mm", "tmin_C", "tmax_C"])
+qsorted=pd.DataFrame(columns = ["water year", "month", "Q_mms"])
 
 for data in range(len(qin["Unnamed: 0"])):
-    qsorted[data,1]=int(qin.loc[data,"Unnamed: 0"][5:7]) #making months useable integers
+    qsorted.loc[data,"month"]=int(qin.loc[data,"Unnamed: 0"][5:7]) #making months useable integers
     if int(qin.loc[data,"Unnamed: 0"][5:7])<10: #to organize by water year 1 Oct - 30 Sep
-        qsorted[data,0]=int(qin.loc[data,"Unnamed: 0"][0:4])-1 #making years useable integers
+        qsorted.loc[data,"water year"]=int(qin.loc[data,"Unnamed: 0"][0:4])-1 #making years useable integers
     else:
-        qsorted[data,0]=int(qin.loc[data,"Unnamed: 0"][0:4]) #making years useable integers
-    qsorted[data,2]=qin.loc[data,"Q_cms"]
+        qsorted.loc[data,"water year"]=int(qin.loc[data,"Unnamed: 0"][0:4]) #making years useable integers
+    qsorted.loc[data,"Q_mms"]=qin.loc[data,"Q_cms"]*10
 
 for data in range(len(pin["Unnamed: 0"])):
-    psorted[data,1]=pin.loc[data,"month"] #no int() needed as the values in this dataframe are already integers
+    psorted.loc[data,"month"]=pin.loc[data,"month"] #no int() needed as the values in this dataframe are already integers
     if pin.loc[data,"month"]<10: #to organize by water year 1 Oct - 30 Sep
-        psorted[data,0]=pin.loc[data,"year"]-1
+        psorted.loc[data,"water year"]=pin.loc[data,"year"]-1
     else:
-        psorted[data,0]=pin.loc[data,"year"]
-    psorted[data,2]=pin.loc[data,"precip_mm"]
-    psorted[data,3]=pin.loc[data,"tmin_C"]
-    psorted[data,4]=pin.loc[data,"tmax_C"]
+        psorted.loc[data,"water year"]=pin.loc[data,"year"]
+    psorted.loc[data,"precip_mm"]=pin.loc[data,"precip_mm"]
+    psorted.loc[data,"tmin_C"]=pin.loc[data,"tmin_C"]
+    psorted.loc[data,"tmax_C"]=pin.loc[data,"tmax_C"]
 
-print(psorted)
-print(qsorted)
+
+df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
+                   columns=['year', 'precip_mm', 'tmin_C'])
+
+#print(psorted)
+#print(qsorted)
 
 """Question 1: Calculate the annual runoff ratio for the Cowichan watershed for
 each year in a selected 10-year period (to clarify: select 10 years with data 
 for both streamflow and precipitation, calculate 10 runoff ratios). 
 Plot the time series of the runoff ratios and discuss how they vary 
 interannually (Hint: use some statistics to back up your answer!)"""
+
+timeframe1=range(1994,2004)
+arratio=pd.Series(index=timeframe1)
+annualp=psorted["precip_mm"].where(psorted["water year"]==1994)
+
+#print(qsorted.where["water year"==1994, "Q_cms"])
+
+
+
+for year in timeframe1:
+    annualp=psorted["precip_mm"].where(psorted["water year"]==year)
+    annualq=qsorted["Q_mms"].where(qsorted["water year"]==year)
+    arratio[year]=annualq.mean()/annualp.mean()
+
+print(arratio)
+
+arratio.plot(kind='bar',rot=0)
+plt.xlabel("Water Years")	
+plt.ylabel("Runoff ratio")
+plt.title("Runoff Ratios for Duncan Glenora from %s " % str(np.amin(timeframe1)) + "to %s" % str(np.amax(timeframe1)))
+#arratio.plot()
+plt.show()
+#title=() )
+
  
 """Question 2: Calculate monthly runoff ratios for two years of data. How do 
 they vary intraannually (within a year) and interannually (across years, for 
 example how do the two January runoff ratios compare)?"""
- 
+
+months=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+timeframe2=[2006,2007]
+temp=range(2)
+mrratio=pd.Series(index=months)
+for m in months:
+    for year in timeframe2:
+        monthlyp=psorted["precip_mm"].where((psorted["water year"]==year) & (psorted["month"]==(months.index(m)+1)))
+        monthlyq=qsorted["Q_mms"].where((qsorted["water year"]==year) & (qsorted["month"]==(months.index(m)+1)))
+        temp[year-np.amin(timeframe2)]=monthlyq.mean()/monthlyp.mean() #there should be a better way...
+        mrratio[m]=temp #still buggy here...
+print(mrratio)
+                        
+                        
 """Question 3: Calculate and plot a monthly water budget for two years of data.
 Discuss how the seasonality of the water budget varies across variables, the 
 intra- and interannual variability, and the sources of uncertainty in the water
