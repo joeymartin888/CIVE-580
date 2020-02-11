595c5af8d77597a2811fea9b0304363b938b94ac
joeymartin888 <joeymartin888@gmail.com>
2020-02-11 11:45:25 -0800

    final submission

--- Assignment3/Assignment3.py
+++ Assignment3/Assignment3.py
@@ -1,8 +1,15 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.figure as fig
+import matplotlib.dates as mdates
 
 up=pd.read_csv("River_routing_data.csv")
 
-time_min=3
+"""Question 1: Route the streamflow from Walnutport to Whitehall, ensuring the 
+Courant condition is met."""
+
+time_min=3 #Change timestep as required to meet Courant condition
 timestep=[]
 j=0
 while j<(60/time_min):
@@ -10,7 +17,7 @@ while j<(60/time_min):
 	j+=1
 
 #print (timestep)
-q=pd.DataFrame(0, index=range(len(up)*len(timestep)-(len(timestep)-1)), columns=["YEAR", "MONTH", "DAY", "HOUR","MINUTES","Qup_cfs","Qdown_cfs"])
+q=pd.DataFrame(0, index=range(len(up)*len(timestep)-(len(timestep)-1)), columns=["YEAR", "MONTH", "DAY", "HOUR","MINUTE","Walnutport","Whitehall"])
 for i in range(len(up)):
 	for t in range(len(timestep)):
 		q.iloc[(i*len(timestep)+t),0]=up.iloc[i,0]
@@ -25,7 +32,7 @@ for i in range(len(up)):
 
 #print (q)
 
-dist_ft=5280
+dist_ft=5280/4
 Length=5280*10
 diststep=[]
 k=0
@@ -39,7 +46,7 @@ qroute=pd.DataFrame(0, index=range(len(q)), columns=diststep)
 qroute.iloc[0,:]=q.iloc[0,5] #set initial conditions
 qroute.iloc[:,0]=q.iloc[:,5]
 step=(time_min*60.0)/dist_ft
-print(step)
+#print(step)
 alpha=4.13 #temp
 beta=0.6 #confirm
 
@@ -52,4 +59,39 @@ for j in range(len(q)-1):
 		denom=step+alpha*beta*((qroute.iloc[j,(i+1)]+qroute.iloc[(j+1),i])/2)**(0.6-1)
 		qroute.iloc[(j+1),(i+1)]=(step*qroute.iloc[(j+1),i]+alpha*beta*qroute.iloc[j,(i+1)]*((qroute.iloc[j,(i+1)]+qroute.iloc[(j+1),i])/2)**(0.6-1))/(step+alpha*beta*((qroute.iloc[j,(i+1)]+qroute.iloc[(j+1),i])/2)**(0.6-1))
 
-print(qroute)
+#print(qroute)
+
+q["Whitehall"]=qroute.iloc[:,(len(qroute.columns)-1)]
+
+"""Question 2: Plot the upstream and downstream hydrographs on the same plot."""
+
+
+newq=pd.DataFrame(columns=["Time", "Walnutport","Whitehall"])
+newq["Time"]=pd.to_datetime(q.iloc[:,0:5])
+newq["Walnutport"]=q["Walnutport"]
+newq["Whitehall"]=q["Whitehall"]
+
+
+x=pd.to_datetime(q.iloc[:,0:5])
+fig, ax = plt.subplots()
+ax.plot(newq["Time"], newq["Walnutport"])
+ax.plot(newq["Time"], newq["Whitehall"])
+plt.xlabel("Time (Day and Hour) - timestep = %i min" % time_min)	
+fig.autofmt_xdate(bottom=None, rotation=30)
+#ax.fmt_xdata = mdates.DateFormatter('%d')
+plt.ylabel("Streamflow (in cfs)")
+plt.title("Stream Routing of Lehigh River from Walnutport to Whitehall")
+plt.legend()
+plt.show()
+
+"""Question 3: What is the speed of the flood wave? You may approximate
+this with the time between the two peaks of the hydrographs."""
+
+flood_up=newq["Time"].where(newq["Walnutport"]==max(newq["Walnutport"]))
+flood_down=newq["Time"].where(newq["Whitehall"]==max(newq["Whitehall"]))
+f=flood_down.dropna()
+g=flood_up.dropna()
+flood_time=f.iloc[0]-g.iloc[0]
+j=flood_time.seconds
+flood_speed=Length/flood_time.seconds
+print("The flood wave travelled at %0.2f cfs." % flood_speed)
